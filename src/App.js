35fb157ebab3344855fede78f23287bfac4d614f
Oke Uguru <ondukwe@gmail.com>
2019-03-09 17:57:47 -0500

    restructured folders

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function App() {
+    return (
+        <div>
+            <input type="checkbox" />
+            <p> Placeholder text here </p>
+        </div>
+    )
+}
+
+export default App
