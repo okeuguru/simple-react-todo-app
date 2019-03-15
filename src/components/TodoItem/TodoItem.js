d5363d456afd07ea5f1fb01202634699f9c194b1
Oke Uguru <ondukwe@gmail.com>
2019-03-15 12:13:07 -0400

    Completed style

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -2,6 +2,10 @@ import React from "react"
 import './style.css'
 
 function TodoItem(props) {
+    const completedStyle = {
+        fontStyle: "italic",
+        color: "#cdcdcd"
+    }
     console.log(props)
     return (
         <div className="todo-item">
@@ -9,7 +13,12 @@ function TodoItem(props) {
                 type="checkbox"
                 checked={props.item.completed}
                 onChange={() => props.handleChange(props.item.id)} />
-            <p>{props.item.text}</p>
+            <p style={
+                props.item.completed ?
+                    completedStyle :
+                    null}>
+                {props.item.text}
+            </p>
         </div>
     )
 }
